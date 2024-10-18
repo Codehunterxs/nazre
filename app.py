b46f9c625587967ac4108674f6e036d2481a02d5
unsack <alambhaixd69@gmail.com>
2024-10-19 04:08:54 +0600

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import json
 import string
 import re
 import random
+import os
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -159,5 +161,7 @@ def process_card():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    # Get the port from the environment variable or default to 5000
+    port = int(os.environ.get('PORT', 5000))
+    # Run the Flask app on the specified port
+    app.run(host='0.0.0.0', port=port)
